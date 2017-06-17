0f08d0e1ce018318658d02acbd0f7c708d064d2e
Santosh Kumar Thakur <santoshthakur100@gmail.com>
2017-06-17 19:34:01 +0530

    HTML Commit

--- app/entry.js
+++ app/entry.js
@@ -1,4 +1,4 @@
-require('./app-module.js');
+require('./app-module');
 require('./components/list-container/list-container-component');
 require('./components/list-item-component/list-item-module');
 require('./components/list-item-component/list-item-component');
